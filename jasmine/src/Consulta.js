507e5764ef6887f107350feaec896ae8f1c463b8
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-08-17 08:29:39 -0300

    Teste com o ecma antigo

--- jasmine/src/Consulta.js
+++ jasmine/src/Consulta.js
@@ -1,40 +1,21 @@
-export class Consulta {
-  constructor (paciente, procedimentos, particular, retorno) {
-    this._paciente = paciente
-    this._procedimentos = procedimentos
-    this._particular = particular
-    this._retorno = retorno
+function Consulta (paciente, procedimentos, particular, retorno) {
+  var clazz = {
+    preco: function () {
+      if (retorno) return 0
+
+      var precoFinal = 0
+
+      procedimentos.forEach(function (procedimento) {
+        if (procedimento === 'raio-x') precoFinal += 55
+        else if (procedimento === 'gesso') precoFinal += 32
+        else precoFinal += 25
+      })
+
+      if (particular) precoFinal *= 2
+
+      return precoFinal
+    }
   }
 
-  get paciente () {
-    return this._paciente
-  }
-
-  get procedimentos () {
-    return this._procedimentos
-  }
-
-  get particular () {
-    return this._particular
-  }
-
-  get retorno () {
-    return this._retorno
-  }
-
-  preco () {
-    if (this.retorno) return 0
-
-    let precoFinal = 0
-
-    this.procedimentos.forEach(function (procedimento) {
-      if (procedimento === 'raio-x') precoFinal += 55
-      else if (procedimento === 'gesso') precoFinal += 32
-      else precoFinal += 25
-    })
-
-    if (this.particular) precoFinal *= 2
-
-    return precoFinal
-  }
-}
+  return clazz
+};
--- jasmine/src/Paciente.js
+++ jasmine/src/Paciente.js
@@ -1,36 +1,19 @@
-export class Paciente {
-  constructor (nome, idade, peso, altura) {
-    this._nome = nome
-    this._idade = idade
-    this._peso = peso
-    this._altura = altura
+function Paciente (nome, idade,
+  peso, altura) {
+  var clazz = {
+
+    imprime: function () {
+      alert(nome + ' tem ' + idade)
+    },
+
+    batimentos: function () {
+      return idade * 365 * 24 * 60 * 80
+    },
+
+    imc: function () {
+      return peso / (altura * altura)
+    }
   }
 
-  get nome () {
-    return this._nome
-  }
-
-  get idade () {
-    return this._idade
-  }
-
-  get peso () {
-    return this._peso
-  }
-
-  get altura () {
-    return this._altura
-  }
-
-  imprime () {
-    alert(this.nome + ' tem ' + this.idade)
-  }
-
-  batimentos () {
-    return this.idade * 365 * 24 * 60 * 80
-  }
-
-  imc () {
-    return this.peso / (this.altura * this.altura)
-  }
+  return clazz
 }
